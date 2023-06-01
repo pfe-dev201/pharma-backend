253f2871bdc1e4afd7363a55f55ca6932a8d4c80
MouadALLAOUI <moadallaoui1@gmail.com>
2023-06-01 18:16:05 +0100

    [PHARMA-36] Back-end Login

--- database/migrations/2023_05_29_000006_ajouter_colonne_a_users.php
+++ database/migrations/2023_05_29_000006_ajouter_colonne_a_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('image_profile')->after('email');
+            $table->string('image_profile')->default('default-profile.jpg')->after('email');
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EntreeMedicamentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResources([
     "entrees" => EntreeMedicamentController::class,
